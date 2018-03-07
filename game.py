1a6f9ed3429f903e67d2d29d430a3ccb1d4463a0
JoRens18 <m185388@usna.edu>
2018-03-07 00:01:43 -0500

    Ship Moves

--- game.py
+++ game.py
@@ -18,28 +18,41 @@ from color import *
 
 pygame.init()
 window = pygame.display.set_mode((1000,1000),0,32)
-
+pygame.display.set_caption('Avoid the Torpedos!')
 #set up FPS
 fpsClock = pygame.time.Clock()
-FPS = 120
+FPS = 60
 
 torpedo = shapes.Torpedo()
+ship = shapes.Ship()
+#submarine = shapes.Submarine()
 
 #Drawing world 
 def draw_world(surf):
   surf.fill(NAVYBLUE)
-  pygame.display.update()
-  torpedo.draw(surf)
+  ship.draw(surf)
+  #submarine.draw(surf)
+  
 
 while(True):
-	draw_world(window)
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			pygame.quit()
-			sys.exit()
+  for event in pygame.event.get():
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_UP:
+        ship.changeSpeed(0,-5)
+      if event.key == pygame.K_DOWN:
+        ship.changeSpeed(0,5)
+      if event.key == pygame.K_RIGHT:
+        ship.changeSpeed(5,0)
+      if event.key == pygame.K_LEFT:
+        ship.changeSpeed(-5,0)
+    if event.type == QUIT:
+      pygame.quit()
+      sys.exit()
+  
+  ship.move()
+  draw_world(window)
+  pygame.display.update()
+  fpsClock.tick(FPS)
 
-
-	pygame.display.update()
-	fpsClock.tick(FPS)
 
 	
--- shapes.py
+++ shapes.py
@@ -1,7 +1,7 @@
 import math
 import pygame, sys
 from pygame.locals import *
-
+import random
 
 class Torpedo(pygame.sprite.Sprite):
   def __init__(self):
@@ -17,23 +17,41 @@ class Torpedo(pygame.sprite.Sprite):
 
 
 class Ship(pygame.sprite.Sprite):
-  def __init__(self,color, width, height):
+  def __init__(self):
     super(Ship,self).__init__()
     pygame.sprite.Sprite.__init__(self)
     self.Vx = 0
     self.Vy = 0
     self.image = pygame.image.load('images/ship.png')
-    self.image = pygame.transform.scale(self.image,(130,57.5))
+    self.image = pygame.transform.scale(self.image,(130,58))
     self.rect = self.image.get_rect()
-    self.rect.center = (750,500)
+    self.x = random.randint(250,751)
+    self.y = random.randint(250,751)
+    self.rect.center = (self.x,self.y)
 
   def draw(self,surf):
     surf.blit(self.image,self.rect)
 
+  def changeSpeed(self,dx,dy):
+    self.Vx += dx
+    self.Vy += dy
+    
+  def move(self):
+    self.x += (self.Vx * (1.0/20))
+    self.y += (self.Vy * (1.0/20))
+    self.rect.center = (self.x,self.y)
+    
+
 class Submarine(pygame.sprite.Sprite):
-  def __init__(self,color, width, height):
+  def __init__(self):
     super(Submarine,self).__init__()
     pygame.sprite.Sprite.__init__(self)
     self.image = pygame.image.load('image/submarine.png')
-    self.rect. = self.image.get_rect()
+    self.rect = self.image.get_rect()
     self.rect.center = (500,750)
+  
+  def draw(self,surf):
+    surf.blit(self.image,self.rect)
+
+
+
