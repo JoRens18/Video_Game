b6d3b731d4d045cf55cd81575ffec4bea4f338b4
thetstone1 <m186252@usna.edu>
2018-03-25 22:16:20 -0400

    sensor update

--- Video_Game_Sensors/Video_Game_Sensors.ino
+++ Video_Game_Sensors/Video_Game_Sensors.ino
@@ -11,58 +11,43 @@ int water_val;     		 //Variable to store the incomming data
 //Buzzer Sound
 const int buzzer = 9; //buzzer to arduino pin 9
 
-//Joystick
-int xposition = 0;
-int yPosition = 0;
-int buttonState = 0;
 
+// RGB LED
+int redPin = 3;
+int greenPin = 5;
+int bluePin = 6;
 
-// RGB Sensor
-int redPin = 7;
-int greenPin = 6;
-int bluePin = 5;
-
+int s_data = 0;
 void setup()
 {
-	//Begin serial communication
-	Serial.begin(9600);
-
 	pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
-
         pinMode(redPin,OUTPUT); 
         pinMode(greenPin,OUTPUT);
-        pinMode(blue,OUTPUT);
-        
-        pinMode(xPin,INPUT);
-        pinMode(yPin,INPUT);
-        pinMode(buttonPin,INPUT_PULLUP);
+        pinMode(bluePin,OUTPUT);
+  //Begin serial communication
+	Serial.begin(9600);
 }
 
 void loop()
 {
+  while(Serial.available() ==0);
+  char c = Serial.read();
+  if (c =='p'){
         tone(buzzer, 1000); // Send 1KHz sound signal...
-	Water_val = analogRead(water); //Read data from analog pin and store it to value variable
-	
-	if (Water_val<=480){ 
+	water_val = analogRead(water); //Read data from analog pin and store it to value variable
+	if (water_val<=50){ 
 		Serial.println("PUT OUT THE FIRE"); 
                 analogWrite(redPin,255);
                 analogWrite(greenPin,0);
                 analogWrite(bluePin,0);
 
 	}
-	else if (Water_val>480 && Water_val < 630){ 
-		Serial.println("Fire Partly Relinquished Keep Going"); 
-                analogWrite(redPin,255);
-                analogWrite(greenPin,255);
-                analogWrite(bluePin,0);
-	}
-        else if (Water_val>480 && Water_val < 630){ 
+	else if (water_val>50){ 
 		Serial.println("Fire Relinquished"); 
                 analogWrite(redPin,0);
                 analogWrite(greenPin,255);
                 analogWrite(bluePin,0);
 	}
+  }
 	
-
-	delay(5000); // Check for new value every 5 sec
 }
